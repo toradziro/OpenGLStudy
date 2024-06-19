9085eb4e5277060e1ba6917d0f79442d679fe67b
ehillman <toradziro@gmail.com>
2024-06-19 22:13:46 +0200

    First Triangle on Screen

--- OpenGlStudy/src/main.cpp
+++ OpenGlStudy/src/main.cpp
@@ -33,17 +33,28 @@ int main(int argc, char** argv)
 	std::cout << "GLFW version: " << glfwGetVersionString() << std::endl;
 	std::cout << "GLEW version: " << glGetString(GL_VERSION) << std::endl;
 
+	float positions[] =
+	{
+		-0.5f,	-0.5f,
+		0.0f,	0.5f,
+		0.5f,	-0.5f
+	};
+
+	uint32_t buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+	glEnableVertexAttribArray(0);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glVertex2f(0.0f, 0.25f);
-		glEnd();
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
