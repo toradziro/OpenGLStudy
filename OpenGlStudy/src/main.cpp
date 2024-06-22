9849fb68bca8792434e16c54428442d9b892e159
ehillman <toradziro@gmail.com>
2024-06-22 20:22:01 +0200

    A Bit More Platform Independent Assertation(in case we would like to run it on other platforms)

--- OpenGlStudy/src/main.cpp
+++ OpenGlStudy/src/main.cpp
@@ -12,7 +12,23 @@ struct ShaderProgramSource
 	std::string m_fragmentSource;
 };
 
-#define ASSERT(x) if (!(x)) { __debugbreak(); }
+#if defined(_MSC_VER)
+	#define DEBUG_BREAK() __debugbreak()
+#elif defined(__GNUC__) || defined(__clang__)
+	#if defined(__i386__) || defined(__x86_64__)
+		#define DEBUG_BREAK() asm volatile("int3")
+	#else
+		#include <csignal>
+		#define DEBUG_BREAK() raise(SIGTRAP)
+	#endif
+#elif defined(__MINGW32__) || defined(__MINGW64__)
+	#define DEBUG_BREAK() __builtin_trap()
+#else
+	#include <cassert>
+	#define DEBUG_BREAK() assert(false)
+#endif
+
+#define ASSERT(x) if (!(x)) { DEBUG_BREAK(); }
 #define GLCall(x) GLCLearError();\
 	x;\
 	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
